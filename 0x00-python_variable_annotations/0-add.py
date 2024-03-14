c4601419c6e9d8eb980b6cbd80e6a8f451ffcbde
olusiekwin <olusiekwin@gmail.com>
2024-03-15 02:06:05 +0300

    🌘 0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module Docs
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Sum 2 floating-points number and returns the result
+
+    Parameters:
+    - a (float): The first float operand.
+    - b (float): The second float operand.
+
+    Returns:
+    float: The sum of a and b.
+    """
+    result: float = a + b
+    return result
